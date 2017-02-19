322b987cefaa1b4dc000e80c9dd60fcb3dfe8c6f
Alexandr <alexlukovkin@gmail.com>
2017-02-19 21:53:55 +0300

    magnets

---
+++ Chapter2/src/DrumKit.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Alexandr on 19.02.2017.
+ */
+public class DrumKit {
+    boolean topHa=true;
+    boolean snare = true;
+
+    void playTopHat(){
+        System.out.println("динь динь ди-динь");
+    }
+    void playSnare(){
+        System.out.println("бах бах ба-бах");
+    }
+}
---
+++ Chapter2/src/DrumKitTestDrive.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Alexandr on 19.02.2017.
+ */
+public class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d=new DrumKit();
+        d.playSnare();
+        d.playTopHat();
+        d.snare=false;
+        if(d.snare==true){
+            d.playSnare();
+        }
+    }
+}
