e6ed62cbcbabeb58d2d3a24b727bf21413a19241
Alexandr Lukovkin <alexlukovkin@gmail.com>
2017-05-25 19:18:30 +0300

    Ch4 Fill World with Dogs!!!!

---
+++ Chapter4/src/MoreDogsTestDrive.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+
+/**
+ * Created by Comp on 25.05.2017.
+ */
+public class MoreDogsTestDrive {
+    public static void main(String[] args) {
+        System.out.println("Hello, this cruel world!");
+        System.out.println("Now I'll fill you with dogs!");
+        Dog dogs[]= new Dog[1];
+        while(true){
+            dogs[0]=new Dog();
+        }
+    }
+}
